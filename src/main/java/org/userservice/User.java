dfc4f55d36660fffc107b133703638ac61cded3d
KonneyJ <konnova.j@yandex.ru>
2025-10-25 14:20:18 +0300

    Добавлены аннотации у класса User

--- src/main/java/org/userservice/User.java
+++ src/main/java/org/userservice/User.java
@@ -1,11 +1,32 @@
 package org.userservice;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "age", nullable = false)
     private short age;
+
+    @Column(name = "created_at")
     private LocalDateTime created = LocalDateTime.now();
 }
