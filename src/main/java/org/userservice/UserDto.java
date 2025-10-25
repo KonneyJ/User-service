c1316ed4f52850ec2ede4f9ad32af51cf2ea270c
KonneyJ <konnova.j@yandex.ru>
2025-10-25 14:44:13 +0300

    Добавлен класс UserMapper

--- src/main/java/org/userservice/UserDto.java
+++ src/main/java/org/userservice/UserDto.java
@@ -1,12 +1,14 @@
 package org.userservice;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserDto {
     private int id;
     private String name;
---
+++ src/main/java/org/userservice/UserMapper.java
@@ -0,0 +1,23 @@
+package org.userservice;
+
+import java.time.LocalDateTime;
+
+public class UserMapper {
+    public static UserDto toUserDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .age(user.getAge())
+                .build();
+    }
+
+    public static User toUser(UserDto user) {
+        return User.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .age(user.getAge())
+                .build();
+    }
+}
