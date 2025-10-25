7d93c0ab04cb0dbeaca2d541beb36792b9c71c8f
KonneyJ <konnova.j@yandex.ru>
2025-10-25 14:34:36 +0300

    Добавлен класс UserDto

---
+++ src/main/java/org/userservice/UserDto.java
@@ -0,0 +1,15 @@
+package org.userservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private int id;
+    private String name;
+    private String email;
+    private short age;
+}
