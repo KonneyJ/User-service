1a94785b45637e82bec86f69d1a566e730567576
KonneyJ <konnova.j@yandex.ru>
2025-11-21 14:49:41 +0300

    Добавлена поддержка HATEOAS, чтобы API предоставляло ссылки для навигации по ресурсам

--- src/main/java/org/userservice/controller/UserController.java
+++ src/main/java/org/userservice/controller/UserController.java
@@ -10,6 +10,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +26,11 @@ import org.userservice.dto.UserDto;
 import org.userservice.service.UserService;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @Slf4j
@@ -40,12 +47,18 @@ public class UserController {
             content = @Content(mediaType = "application/json",
                     schema = @Schema(implementation = UserDto.class)))
     @ApiResponse(responseCode = "400", description = "Некорректные данные")
-    public UserDto createUser(
+    public EntityModel<UserDto> createUser(
             @Parameter(description = "Данные пользователя для создания", required = true)
             @RequestBody UserDto user) {
         log.info("POST /users запрос with UserDto {}", user);
 
-        return userService.createUser(user);
+        UserDto userCreated = userService.createUser(user);
+
+        EntityModel<UserDto> resource = EntityModel.of(userCreated);
+
+        resource.add(linkTo(methodOn(UserController.class).getUserById(userCreated.getId())).withSelfRel());
+
+        return resource;
     }
 
     @PatchMapping("/{id}")
@@ -71,13 +84,20 @@ public class UserController {
                     description = "Пользователь с указанным ID не найден"
             )
     })
-    public UserDto updateUser(@Parameter(description = "ID пользователя, которого нужно обновить", required = true)
-                              @PathVariable int id,
-                              @Parameter(description = "Поля пользователя для обновления", required = true)
-                              @RequestBody UserDto user) {
+    public EntityModel<UserDto> updateUser(@Parameter(description = "ID пользователя, которого нужно обновить", required = true)
+                                           @PathVariable int id,
+                                           @Parameter(description = "Поля пользователя для обновления", required = true)
+                                           @RequestBody UserDto user) {
         log.info("PATCH /users/id запрос with id {}, UserDto {}", id, user);
 
-        return userService.updateUser(id, user);
+        UserDto userUpdated = userService.updateUser(id, user);
+
+        EntityModel<UserDto> resource = EntityModel.of(userUpdated);
+
+        resource.add(linkTo(methodOn(UserController.class).getUserById(id)).withSelfRel());
+        resource.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("collection"));
+
+        return resource;
     }
 
     @DeleteMapping("/{id}")
@@ -99,11 +119,17 @@ public class UserController {
                     description = "Пользователь с указанным ID не найден"
             )
     })
-    public boolean deleteUser(@Parameter(description = "ID пользователя для удаления", required = true)
-                              @PathVariable int id) {
+    public EntityModel<Boolean> deleteUser(@Parameter(description = "ID пользователя для удаления", required = true)
+                                           @PathVariable int id) {
         log.info("DELETE /users/id запрос with id {}", id);
 
-        return userService.deleteUser(id);
+        boolean wasDeleted = userService.deleteUser(id);
+
+        EntityModel<Boolean> resource = EntityModel.of(wasDeleted);
+
+        resource.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("collection"));
+
+        return resource;
     }
 
     @GetMapping("/{id}")
@@ -125,11 +151,18 @@ public class UserController {
                     description = "Пользователь с указанным ID не найден"
             )
     })
-    public UserDto getUserById(@Parameter(description = "ID запрашиваемого пользователя", required = true)
-                               @PathVariable int id) {
+    public EntityModel<UserDto> getUserById(@Parameter(description = "ID запрашиваемого пользователя", required = true)
+                                            @PathVariable int id) {
         log.info("GET /users запрос with id {}", id);
 
-        return userService.getUserById(id);
+        UserDto user = userService.getUserById(id);
+
+        EntityModel<UserDto> resource = EntityModel.of(user);
+
+        resource.add(linkTo(methodOn(UserController.class).getUserById(id)).withSelfRel());
+        resource.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("collection"));
+
+        return resource;
     }
 
     @GetMapping
@@ -147,9 +180,21 @@ public class UserController {
                     )
             )
     })
-    public Collection<UserDto> getAllUsers() {
+    public CollectionModel<EntityModel<UserDto>> getAllUsers() {
         log.info("GET /users запрос");
 
-        return userService.getAllUsers();
+        Collection<UserDto> users = userService.getAllUsers();
+
+        List<EntityModel<UserDto>> resources = users.stream()
+                .map(user -> {
+                    EntityModel<UserDto> resource = EntityModel.of(user);
+                    resource.add(linkTo(methodOn(UserController.class)
+                            .getUserById(user.getId())).withSelfRel());
+                    return resource;
+                })
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(resources,
+                linkTo(methodOn(UserController.class).getAllUsers()).withSelfRel());
     }
 }
